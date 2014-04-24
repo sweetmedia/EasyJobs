96f2521ad1d869849fac17ba557530e1c941cfe4
Juan Perez <juanto@wearesweetmedia.com>
2014-04-24 18:13:05 -0400

    Rails files initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Easyjobs::Application.config.secret_key_base = '086c0dccdc551b5c11fa8e841017cc10f75dc7a263727e1517d043f623d5680802ba4dccf3cbac5a944aa427af787df7c4f611adb499ecc75056ddcea32a6d07'
