dc151965bbcbd035f01ab6fcad391e3a2bd0208f
Juan Perez <juanto@wearesweetmedia.com>
2014-04-25 16:28:15 -0400

    Set root to Jobs index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Easyjobs::Application.routes.draw do
   resources :jobs
 
+  root :to => 'jobs#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
